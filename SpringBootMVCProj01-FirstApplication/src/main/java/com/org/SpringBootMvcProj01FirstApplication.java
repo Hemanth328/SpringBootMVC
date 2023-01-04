0f1f9f76e202f51e2d46b035d2071ec6819c474e
Hemanth328 <hemanth.blueburn11@gmail.com>
2023-01-04 07:23:21 +0530

    MVC Sample Application

---
+++ SpringBootMVCProj01-FirstApplication/src/main/java/com/org/SpringBootMvcProj01FirstApplication.java
@@ -0,0 +1,12 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcProj01FirstApplication {
+	
+	 public static void main(String[] args) {
+	  SpringApplication.run(SpringBootMvcProj01FirstApplication.class, args);
+	 }
+}
