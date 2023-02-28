3770d9918b7a74685b4a5baef675ad5ac32f1ac5
Hemanth328 <hemanth.blueburn11@gmail.com>
2023-02-28 08:19:27 +0530

    I18n

---
+++ SpringBootMVCProj15-I18n/src/main/java/com/org/SpringBootMvcProj15I18nApplication.java
@@ -0,0 +1,13 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcProj15I18nApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcProj15I18nApplication.class, args);
+	}
+
+}
