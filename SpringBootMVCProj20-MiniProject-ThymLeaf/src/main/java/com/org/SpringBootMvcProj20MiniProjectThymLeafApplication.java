d22bdcbd9029a9a899548ad1658a8146712949d4
Hemanth328 <hemanth.blueburn11@gmail.com>
2023-03-25 08:42:55 +0530

    MiniProject using Thymleaf

---
+++ SpringBootMVCProj20-MiniProject-ThymLeaf/src/main/java/com/org/SpringBootMvcProj20MiniProjectThymLeafApplication.java
@@ -0,0 +1,13 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcProj20MiniProjectThymLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcProj20MiniProjectThymLeafApplication.class, args);
+	}
+
+}
